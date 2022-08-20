917a062859d73a90402da10f3b9f8ed3ad53445b
khaitran <akashikazuo99@gmail.com>
2022-08-20 15:15:29 +0700

    new project

---
+++ src/components/HeaderHome/HeaderHome.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+export default function HeaderHome() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <NavLink className="navbar-brand" to="">
+          Project Hook
+        </NavLink>
+        <button
+          className="navbar-toggler d-lg-none"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavId"
+          aria-controls="collapsibleNavId"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        />
+        <div className="collapse navbar-collapse" id="collapsibleNavId">
+          <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="">
+                Home <span className="visually-hidden">(current)</span>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="">
+                Link
+              </NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="dropdownId"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Hooks
+              </a>
+              <div className="dropdown-menu" aria-labelledby="dropdownId">
+                <NavLink className="dropdown-item" to="/useState">
+                  Hook UseState
+                </NavLink>
+              </div>
+            </li>
+          </ul>
+          <form className="d-flex my-2 my-lg-0">
+            <input
+              className="form-control me-sm-2"
+              type="text"
+              placeholder="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+}
